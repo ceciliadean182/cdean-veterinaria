930fb47b606af2f1b7eda8f7f67d8d43f18426f0
Cecilia <cecilia.deanm@gmail.com>
2021-10-20 21:23:01 -0300

    Icon Cart added

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+//import carrito from '../../src/shopping-cart.png'
+import { FaShoppingCart } from 'react-icons/fa';
+const CartWidget = () => {
+    return(
+        <FaShoppingCart />
+    )
+}
+
+export default CartWidget;
